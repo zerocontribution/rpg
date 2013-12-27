14564ad4a6af941a9899e2947b30892830400022
Rob Zienert <rzienert@bloomhealthco.com>
2013-12-27 14:33:17 -0600

    initial commit

---
+++ desktop/src/main/java/io/zerocontribution/winter/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.zerocontribution.winter;
+
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class DesktopLauncher {
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new WinterApp(), config);
+    }
+}
